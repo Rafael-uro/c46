2e0a082d553fcfb10cf8d5c2be637f0645f0c492
Rafael-uro <94479855+Rafael-uro@users.noreply.github.com>
2022-05-31 16:17:03 -0300

    Add files via upload

---
+++ inimigos.js
@@ -0,0 +1,14 @@
+class Inimigo{
+    constructor(x,y){
+    this.x = x
+    this.y = y
+    this.body = createSprite(x,y)
+        this.image = loadImage("obstacle1.png")
+        this.body.addImage(this.image)
+        this.body.scale = 0.2
+        this.body.debug = true
+    }
+display(){
+
+}
+}
